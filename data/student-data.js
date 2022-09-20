5ce76670a67ca5edbd7b03a57aa40405a99ee3c6
julianboyne <julianboyne11@gmail.com>
2022-09-19 20:12:04 -0400

    home-page-view

---
+++ data/student-data.js
@@ -0,0 +1,10 @@
+const students = [
+  {name: "Jeff", exam: 7, passed: true},
+  {name: "Julian", exam: 9, passed: true},
+  {name: "Luna", exam: 4, passed: false},
+  {name: "George", exam: 2, passed: false}
+]
+
+export {
+  students
+}
